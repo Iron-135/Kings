cccec94e240e169717c4f252c240b60823d1636f
Iron135 <53490360+Iron-135@users.noreply.github.com>
2021-12-16 12:12:47 -0500

    Add files via upload

---
+++ PlayerDamage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerDamage : NetworkBehaviour
+{
+    [SerializeField] float attackDamage = 50f;
+
+    [Command]
+    public void CmdAttack(GameObject player)
+    {
+        player.GetComponent<PlayerHealth>().currentHealth -= attackDamage;
+    }
+}
---
+++ SwordCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollider : MonoBehaviour
+{
+    [SerializeField] PlayerDamage playerDamage;
+
+    void Start()
+    {
+        //playerDamage = GetComponentInParent<PlayerDamage>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject target = other.gameObject;
+
+        if (target != null)
+        {
+            Debug.Log("Trigger Entered other");
+            playerDamage.CmdAttack(target);
+        }
+        else return;
+        
+    }
+}
